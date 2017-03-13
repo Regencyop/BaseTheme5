26a8879458b0180abe5ef0bd2dfc4d8ce34cff2f
Regency Business Solutions <GitHub@regencyop.com>
2017-03-13 13:24:29 -0400

    Update app.js
    with AddtoCartPreview

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
 'use strict';
 
 var four51 = {};
-four51.app = angular.module('451order', ['ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'ui.bootstrap', 'angulartics', 'angulartics.google.analytics', 'ngAutocomplete', 'tabsView', 'OrderCloud-ProductMatrix', 'OrderCloud-SpecForms', 'OrderCloud-BackToTop', 'OrderCloud-Carousel', 'OrderCloud-ProductLightbox']);
+four51.app = angular.module('451order', ['ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap', 'angulartics', 'angulartics.google.analytics', 'ngAutocomplete', 'OrderCloud-BackToTop', 'OrderCloud-AddToCartPreview', 'OrderCloud-SpecForms', 'OrderCloud-ProductLightbox']);
